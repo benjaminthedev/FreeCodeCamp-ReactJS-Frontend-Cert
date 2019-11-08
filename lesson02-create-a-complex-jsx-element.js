01745c14e4241a7fcc80349ec840b553dbd8c4be
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-11-08 10:26:25 +0000

    new lessons

---
+++ lesson02-create-a-complex-jsx-element.js
@@ -0,0 +1,12 @@
+//create-a-complex-jsx-element
+
+// write your code here
+const JSX = <div>
+    <h1>heading</h1>
+    <p>Paragraph</p>
+    <ul>
+        <li>One</li>
+        <li>Two</li>
+        <li>Three</li>
+    </ul>
+</div>;
--- lesson03-add-comments-in-jsx.js
+++ lesson03-add-comments-in-jsx.js
@@ -1,2 +1,9 @@
 //add-comments-in-jsx
 
+const JSX = (
+    <div>
+        {/*Comment*/}
+        <h1>This is a block of JSX</h1>
+        <p>Here's a subtitle</p>
+    </div>
+);
